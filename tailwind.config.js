62886227c80f5f904f43e9473897fa49f2e4a3d7
Abraham Peter <theclassical001@gmail.com>
2022-01-12 19:47:59 +0100

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    // './src/style.css',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+}
